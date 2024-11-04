2c9c0761dc2acfbd44e08bbcd22cda8358ac8d30
Otabek.E <otabekjan.eshpulatov@gmail.com>
2024-11-04 15:23:29 +0100

    Initial Commit

---
+++ git-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  git-demo
+//
+//  Created by kebato OS on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ git-demo/git_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_demoApp.swift
+//  git-demo
+//
+//  Created by kebato OS on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct git_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
